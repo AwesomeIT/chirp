9c11424b792de41b0f62793e3e1bb15da83c580b
David Stancu <dstancu@nyu.edu>
2016-10-11 21:38:07 -0400

    finish integrating slick and evolution migrations
    [#131965229, #131986235]

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,39 @@
+# --- !Ups
+CREATE TABLE "chirp"."experiment" (
+  "id" serial NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "start_date" date NOT NULL,
+  "end_date" date NOT NULL,
+  "created_at" date NOT NULL,
+  "updated_at" date NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "chirp"."sample" (
+  "id" serial NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "user_id" integer NOT NULL, -- references user("id") DTO TBD
+  "s3_url" varchar(255) NOT NULL,
+  "created_at" date NOT NULL,
+  "updated_at" date NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "chirp"."sample_experiment" (
+  "id" serial NOT NULL,
+  "sample_id" integer references sample(id) NOT NULL,
+  "experiment_id" integer references experiment(id) NOT NULL,
+  PRIMARY KEY("id")
+);
+
+CREATE TABLE "chirp"."score" (
+  "id" serial NOT NULL,
+  -- Scores are scored 0.00 -> 1.00
+  "score" decimal(2, 1) NOT NULL,
+  "sample_experiment_id" integer references sample_experiment(id) NOT NULL,
+  "user_id" integer NOT NULL, -- references user("id") DTO TBD
+  PRIMARY KEY ("id")
+);
+
+# --- !Downs
+DROP SCHEMA "chirp" CASCADE;
