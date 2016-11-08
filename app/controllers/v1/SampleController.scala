0770bb32134e15c963714517232940836440ccc3
David Stancu <davidstancu@toasterspam.hubble.lan>
2016-11-07 23:44:31 -0500

    update namespace in controllers, add basic file uploading logic
    TODO: test, and spy to ensure our S3 trait is called
    
    [#131966481]

--- app/controllers/v1/SampleController.scala
+++ app/controllers/v1/SampleController.scala
@@ -1,34 +1,36 @@
 package controllers.v1
 
 import akka.actor.ActorSystem
+import com.amazonaws.services.s3.model.ObjectMetadata
 import com.google.inject._
 import org.birdfeed.chirp.database.Query
-import org.birdfeed.chirp.database.models.Sample
-import org.birdfeed.chirp.helpers.EndpointHandler
+import org.birdfeed.chirp.support.adapter.S3
+import org.birdfeed.chirp.support.api.EndpointHandler
 import play.api.db.slick.DatabaseConfigProvider
-import play.api.libs.functional.syntax._
-import play.api.libs.json._
 import play.api.mvc._
 import slick.driver.JdbcProfile
 
 import scala.concurrent._
-import scala.util._
 
 @Singleton
-class SampleController @Inject()(actorSystem: ActorSystem, val dbConfigProvider: DatabaseConfigProvider)(implicit exec: ExecutionContext) extends Controller with EndpointHandler with Query {
+class SampleController @Inject()(actorSystem: ActorSystem, val dbConfigProvider: DatabaseConfigProvider)(
+  implicit exec: ExecutionContext
+) extends Controller with EndpointHandler with Query with S3 {
 
   val dbConfig = dbConfigProvider.get[JdbcProfile]
 
-  def create = Action.async(BodyParsers.parse.json) { request =>
-    val createReads: Reads[Future[Try[Sample]]] = (
-        (JsPath \ "name").read[String] and
-        (JsPath \ "user_id").read[Int] and
-        (JsPath \ "s3_url").read[String]
-    )((name: String, userId: Int, s3Url: String) => {
-      Sample.create(name, userId, s3Url)
-    })
-
-    dtoWithMarshallingSingle(createReads, request.body, Created)
+  def create(fileName: String, userId: Int) = Action.async(parse.raw) { request =>
+    request.body.asBytes(2048) match {
+      case Some(bytes) => {
+        dtoWithErrorHandlingSingle(
+          Sample.create(
+            fileName,
+            userId,
+            bucket.putObject(fileName, bytes.toArray, new ObjectMetadata).key
+          ), Created)
+      }
+      case None => Future(InternalServerError(jsonError("S3 file upload failed. Please try again")))
+    }
   }
 
   def retrieve(id: String) = Action.async { request =>
