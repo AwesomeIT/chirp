8c9634f3238f3ef52f92ac7624a7cf0cd71479da
David Stancu <davidstancu@toasterspam.local>
2016-11-10 21:25:25 -0500

    finish tests, need to fix issue with stupid scalamock [#131966481]

--- test/controllers/v1/SampleControllerSpec.scala
+++ test/controllers/v1/SampleControllerSpec.scala
@@ -1,18 +1,20 @@
 package controllers.v1
 
+import awscala.s3.{Bucket, PutObjectResult}
 import org.scalatestplus.play._
 import play.api.libs.json._
 import play.api.libs.ws.WSClient
 import play.api.db.slick.DatabaseConfigProvider
-
 import slick.driver.JdbcProfile
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-
 import org.birdfeed.chirp.database.Query
+import org.birdfeed.chirp.support.adapter._
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{BeforeAndAfter, MustMatchers, WordSpec}
 
-class SampleControllerSpec extends PlaySpec with OneServerPerSuite with Query {
+class SampleControllerSpec extends PlaySpec with OneServerPerSuite with Query with MockFactory with BeforeAndAfter {
   val wsClient = app.injector.instanceOf[WSClient]
 
   val dbConfigProvider = app.injector.instanceOf(classOf[DatabaseConfigProvider])
@@ -23,18 +25,22 @@ class SampleControllerSpec extends PlaySpec with OneServerPerSuite with Query {
     java.util.UUID.randomUUID.toString, s"${uuid}@uuid.com", uuid, 1
   ), Duration.Inf).get
 
+  val payload = "your binary here".getBytes
+  lazy val created = Await.result(
+    wsClient
+      .url(s"http://localhost:${port}/v1/sample")
+      .withQueryString(
+        "user_id" -> user.id.toString,
+        "file_name" -> "scala_test.wav"
+      ).put(payload), Duration.Inf)
+
   "PUT /v1/sample/create" should {
-    lazy val created = Await.result(
-      wsClient
-        .url(s"http://localhost:${port}/v1/sample")
-        .put(Json.obj(
-          "name" -> "testSound",
-          "user_id" -> user.id,
-          "s3_url" -> "woof.mp3"
-        )), Duration.Inf)
+    "create a new sample" in {
+      created.status must equal(201)
+    }
+  }
 
-    "create a new sample" in { created.status must equal(201) }
-
+  "GET /v1/sample/:id" should {
     "retrieve a created sample" in {
       lazy val retrieved = Await.result(
         wsClient
@@ -44,7 +50,9 @@ class SampleControllerSpec extends PlaySpec with OneServerPerSuite with Query {
 
       retrieved.status must equal(200)
     }
+  }
 
+  "DELETE /v1/sample/:id" should {
     "delete a created sample" in {
       Await.result(
         wsClient.url(s"http://localhost:${port}/v1/sample/${(created.json \ "id").get}").delete,
